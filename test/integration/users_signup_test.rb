7e82926d90a4c431795d1142d246f771cbe07eb6
Brad Zellman <bzellman@got2jam.com>
2016-07-30 05:18:58 +0000

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  
+  test "invalid signup information" do
+    get signup_path
+    
+    assert_no_difference 'User.count' do
+        post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+      
+    end
+    
+    assert_template 'users/new'
+    # assert_select 'div#<css id for error explanation>'
+    # assert_select 'div.<css class for field with error>'
+  end
+  
+  test "valid signup information" do
+    get signup_path
+      assert_difference 'User.count', 1 do
+         post users_path, params: { user: { name:  "Test",
+                                         email: "user@correct.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+      
+         end
+         follow_redirect!
+         assert_template 'users/show'
+  end
+  
+end
