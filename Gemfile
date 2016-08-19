16c990927f80e4b0e685589311384e55cca5d815
Brad Zellman <bzellman@got2jam.com>
2016-08-19 20:52:22 +0000

    Add Email and Activation

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'coffee-rails', '4.2.1'
 gem 'jquery-rails', '4.1.1'
 gem 'turbolinks',   '5.0.0'
 gem 'jbuilder',     '2.4.1'
+gem 'sprockets', '3.6.3'
 
 group :development, :test do
   gem 'sqlite3', '1.3.11'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :admin_user,     only: [:destroy]
     
   def index
     @users = User.paginate(page: params[:page])
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in(@user)
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to actiavte your account"
+      redirect_to root_url
     else
       render 'new'
     end
@@ -34,6 +34,12 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User Deleted"
+    redirect_to users_url
   end
   
   def edit
@@ -59,11 +65,6 @@ class UsersController < ApplicationController
     redirect_to(root_url) unless current_user?(@user)
   end
 
-  def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User Deleted"
-    redirect_to users_url
-  end
   
   def admin_user
     redirect_to(root_url) unless current_user.admin?
