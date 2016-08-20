154a624b47a9ddcd8262eb3104563ebb812662ab
Brad Zellman <bzellman@got2jam.com>
2016-08-20 00:56:16 +0000

    change host to include https

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,7 +53,7 @@ class User < ApplicationRecord
     end
     
     def send_activation_email
-         UserMailer.account_activation(self).deliver_now
+        UserMailer.account_activation(self).deliver_now
     end
 
     
