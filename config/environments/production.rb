230232aa23aeeefe4324bd86613e4ad8be7a51a4
Brad Zellman <bzellman@got2jam.com>
2016-08-20 00:43:41 +0000

    change creditentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
+      :user_name      => 'app53017829@heroku.com',
+      :password       => '1sd5czsn2757',
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
     }
