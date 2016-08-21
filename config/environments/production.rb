b619a4b4284ac6fcc757e5347e418d3a22ecd3ee
Brad Zellman <bzellman@got2jam.com>
2016-08-21 18:49:01 +0000

    remove https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => 'app53017829@heroku.com',
-      :password       => '1sd5czsn2757',
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
     }
